c869202d583ffaace88f8e39975bbc86991609ab
friendlyanon <friendlyanon@users.noreply.github.com>
2021-04-01 14:23:37 +0200

    Add developer mode
    Developer mode allows proper separation of concerns by making targets
    available only for the developer(s) of the library that are only
    relevant to the developer(s) of the library.

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,3 +1,19 @@
+string(
+    COMPARE EQUAL
+    "${CMAKE_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}"
+    PROJECT_IS_TOP_LEVEL
+)
+
+# ---- Developer mode ----
+
+if(PROJECT_IS_TOP_LEVEL)
+  option(refl-cpp_DEVELOPER_MODE "Enable developer mode" OFF)
+
+  if("$ENV{CI}")
+    set(refl-cpp_DEVELOPER_MODE ON CACHE INTERNAL "")
+  endif()
+endif()
+
 # ---- Warning guard ----
 
 # Protect dependents from this project's warnings if the guard isn't disabled
