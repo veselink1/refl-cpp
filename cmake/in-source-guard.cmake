a9e19b12d9b71757450085fef0f755d2a38c7992
Veselin Karaganev <vesko.karaganev@gmail.com>
2021-04-04 21:48:00 +0300

    Change in-source builds error message

--- cmake/in-source-guard.cmake
+++ cmake/in-source-guard.cmake
@@ -1,6 +1,6 @@
 if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
   message(FATAL_ERROR "\
-In-source builds are not supported. \
-Delete the generated CMake files and try again with an out-of-source build \
-location.")
+FATAL: In-source builds are not supported. \n\
+Make a new directory (e.g., 'build/'), and run CMake from there. \
+You may need to delete 'CMakeCache.txt' and 'CMakeFiles/'.")
 endif()
