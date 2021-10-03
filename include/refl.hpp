a5f89b4525dc4993e3ba7edd121178a1f403b287
Robert Patterson <robert@robertgpatterson.com>
2021-10-03 11:30:28 -0500

    remove comment of old code

--- include/refl.hpp
+++ include/refl.hpp
@@ -204,8 +204,7 @@ namespace refl
             constexpr auto substr() const noexcept
             {
                 static_assert(Pos <= N);
-                //constexpr size_t NewSize = std::min(Count, N - Pos);
-                constexpr size_t NewSize = ( Count < N - Pos ) ? Count : N - Pos;
+                constexpr size_t NewSize = ( Count < N - Pos ) ? Count : N - Pos; //Visual Studio 2019 croaks on std::min
 
                 char buf[NewSize + 1]{};
                 for (size_t i = 0; i < NewSize; i++) {
